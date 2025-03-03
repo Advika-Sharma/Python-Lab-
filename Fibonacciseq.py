365ea1d11f380807b3353887951b6645329d3217
Advika Sharma <sharmaadvikaa@gmail.com>
2025-03-03 08:24:12 +0530

    exp 5

---
+++ Fibonacciseq.py
@@ -0,0 +1,17 @@
+limit = 4000000 
+if (limit < 2): 
+    print("Sum=0") 
+   
+else: 
+    ef1 = 0 
+    ef2 = 2 
+    sm= ef1 + ef2  
+       
+    while (ef2 <= limit): 
+        ef3 = 4 * ef2 + ef1 
+        if (ef3 > limit):  
+            break 
+        ef1 = ef2  
+        ef2 = ef3  
+        sm = sm + ef2  
+    print("Sum=",sm) 
